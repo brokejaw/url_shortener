afc3ed752fc6808d37d80ebbbd4f174a389a82fb
stephen brokaw <brokaw.stephen@gmail.com>
2014-09-26 15:38:24 -0700

    add controller and model rspec

--- spec/controllers/shortened_urls_controller_spec.rb
+++ spec/controllers/shortened_urls_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ShortenedUrlsController, :type => :controller do
 
+  describe 'GET #root' do
+    it 'renders the submission form' do
+      get :root
+        expect(response).to render_template(:form)
+    end
+  end
 end
